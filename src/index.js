f8acf3d93af704a7f71cf03d827d83ec63a73d2e
Kim Holland <tripleaxis@gmail.com>
2017-01-30 19:14:34 +1100

    Added react-a11y for accessibility debugging

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import a11y from 'react-a11y';
 
 import CheckoutView from './view/checkout';
 
 import priceRules from './core/priceRules.json';
 import model from './core/model';
 
+// enable accessibility debugging for development:
+a11y(React);
+
 // should be loaded from secure location:
 model.init(priceRules);
-
 
 ReactDOM.render(
   <CheckoutView/>,
